4fbbdab605e3712f530968f432cfdc69dedfb933
crr1c4 <german1c4@gmail.com>
2023-06-21 00:17:54 -0600

    match changed to unwrap_or_else

--- src/main.rs
+++ src/main.rs
@@ -8,10 +8,11 @@ fn main() {
     let mut chunk = Chunk::new();
     // ====================================================
     chunk.write(OpCode::Constant(1.2), 123);
-    chunk.write(OpCode::Negate, 123);
+    // chunk.write(OpCode::Negate, 123);
     chunk.write(OpCode::Constant(3.8), 123);
     chunk.write(OpCode::Subtract, 123);
     chunk.write(OpCode::Return, 123);
+    // chunk.write(OpCode::Return, 123);
     // chunk.write(OpCode::Return, 123);
 
 
--- src/vm/mod.rs
+++ src/vm/mod.rs
@@ -2,6 +2,7 @@ use crate::chunk::op_code::OpCode;
 use crate::chunk::value::Value;
 use crate::chunk::Chunk;
 use interpret_result::InterpretResult;
+use std::error::Error;
 
 pub mod interpret_result;
 
@@ -24,7 +25,7 @@ impl VirtualMachine {
         }
     }
 
-    pub fn interpret(&mut self) -> InterpretResult {
+    pub fn interpret(&mut self) -> Result<InterpretResult, Box<dyn Error>> {
         for code in self.chunk.get_codes() {
             match code {
                 OpCode::Constant(value) => self.stack.push(*value),
@@ -33,22 +34,12 @@ impl VirtualMachine {
                     None => panic!("Empty stack!"),
                 },
                 OpCode::Negate => {
-                    let value = match self.stack.pop() {
-                        Some(value) => value,
-                        None => panic!("Empty stack!"),
-                    };
+                    let value = self.stack.pop().unwrap_or_else(|| panic!("Empty stack"));
                     self.stack.push(-value);
                 }
                 OpCode::Add | OpCode::Subtract | OpCode::Multiply | OpCode::Divide => {
-                    let a = match self.stack.pop() {
-                        Some(value) => value,
-                        None => panic!("Empty stack!"),
-                    };
-
-                    let b = match self.stack.pop() {
-                        Some(value) => value,
-                        None => panic!("Empty stack!"),
-                    };
+                    let a = self.stack.pop().unwrap_or_else(|| panic!("Empty stack"));
+                    let b = self.stack.pop().unwrap_or_else(|| panic!("Empty stack"));
 
                     let c = match code {
                         OpCode::Add => binary_operation!(a, b , +),
@@ -63,59 +54,58 @@ impl VirtualMachine {
             }
         }
 
-        InterpretResult::Ok
+        Ok(InterpretResult::Ok)
     }
 }
 
-    // fn push(&mut self, value: Value) {
-    //     self.stack.push(value);
-    // }
+// fn push(&mut self, value: Value) {
+//     self.stack.push(value);
+// }
 
-    // fn pop(&mut self) -> Value {
-    //     match self.stack.pop() {
-    //         Some(value) => value,
-    //         None => panic!("Empty stack!"),
-    //     }
-    // }
+// fn pop(&mut self) -> Value {
+//     match self.stack.pop() {
+//         Some(value) => value,
+//         None => panic!("Empty stack!"),
+//     }
+// }
 
-    
-    // for code in codes.iter() {
-    //     match code {
-    //         OpCode::Constant(value) => self.push(*value),
-    //         OpCode::Negate => {
-    //             let value = -self.pop();
-    //             self.push(value);
-    //         }
-    //         OpCode::Add | OpCode::Subtract | OpCode::Multiply | OpCode::Divide => {
-    //            ;
-    //         },
-    //         OpCode::Return => {
-    //             match self.stack.pop() {
-    //                 Some(value) => println!("{}", value),
-    //                 None => panic!("Empty stack!"),
-    //             }
-    //             return InterpretResult::Ok;
-    //         } // _ => self.binary_operation(instruction),
-    //     }
+// for code in codes.iter() {
+//     match code {
+//         OpCode::Constant(value) => self.push(*value),
+//         OpCode::Negate => {
+//             let value = -self.pop();
+//             self.push(value);
+//         }
+//         OpCode::Add | OpCode::Subtract | OpCode::Multiply | OpCode::Divide => {
+//            ;
+//         },
+//         OpCode::Return => {
+//             match self.stack.pop() {
+//                 Some(value) => println!("{}", value),
+//                 None => panic!("Empty stack!"),
+//             }
+//             return InterpretResult::Ok;
+//         } // _ => self.binary_operation(instruction),
+//     }
 
-    // fn binary_operation(&mut self, op_code: &OpCode) {
-    //     let a = match self.stack.pop() {
-    //         Some(v) => v,
-    //         None => panic!("Empty stack!"),
-    //     };
+// fn binary_operation(&mut self, op_code: &OpCode) {
+//     let a = match self.stack.pop() {
+//         Some(v) => v,
+//         None => panic!("Empty stack!"),
+//     };
 
-    //     let b = match self.stack.pop() {
-    //         Some(v) => v,
-    //         None => panic!("Empty stack!"),
-    //     };
+//     let b = match self.stack.pop() {
+//         Some(v) => v,
+//         None => panic!("Empty stack!"),
+//     };
 
-    //     let c = match op_code {
-    //         OpCode::Add => a + b,
-    //         OpCode::Divide => a / b,
-    //         OpCode::Subtract => a - b,
-    //         OpCode::Multiply => a * b,
-    //         _ => unreachable!(),
-    //     };
+//     let c = match op_code {
+//         OpCode::Add => a + b,
+//         OpCode::Divide => a / b,
+//         OpCode::Subtract => a - b,
+//         OpCode::Multiply => a * b,
+//         _ => unreachable!(),
+//     };
 
-    //     self.stack.push(c);
-    // }
+//     self.stack.push(c);
+// }
